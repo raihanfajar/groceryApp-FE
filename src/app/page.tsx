2f26a1def4e664bdac723c1713b70746197237c1
raihanfajar <rhnfjr@gmail.com>
2025-09-20 22:16:57 +0700

    chore: merging with main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
-import BenefitBanner from "@/components/HomePage/benefitBanner/BenefitBanner";
-import CategoryCarousel from "@/components/HomePage/categoryCarousel/CategoryCarousel";
-import { ProductList } from "@/components/HomePage/productDisplay/ProductList";
-import PromoCarousel from "@/components/HomePage/promoCarousel/PromoCarousel";
+import BenefitBanner from "@/components/homePage/benefitBanner/BenefitBanner";
+import CategoryCarousel from "@/components/homePage/categoryCarousel/CategoryCarousel";
+import { ProductList } from "@/components/homePage/productDisplay/ProductList";
+import PromoCarousel from "@/components/homePage/promoCarousel/PromoCarousel";
 
 // Import data dengan path yang sudah dikoreksi
 import {
   carouselItems,
   dummyRecommendedProducts,
-} from "@/components/HomePage/mapData";
+} from "@/components/homePage/mapData";
 import CustomBorder from "@/components/HomePage/CustomBorder";
 
 export default function HomePage() {

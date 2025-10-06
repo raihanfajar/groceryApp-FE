c068346a9e018991d7ec0b7b14d07fb42f78ab38
raihanfajar <rhnfjr@gmail.com>
2025-10-06 19:48:13 +0700

    fix: fixing name for env

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: process.env.VITE_API_URL || 'http://localhost:8000'
+    baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000'
 })
